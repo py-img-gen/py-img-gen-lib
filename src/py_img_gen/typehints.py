f780e45a7733193aeec6176c2d12e6ef4706c424
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2025-02-16 15:40:44 +0900

    update

--- src/py_img_gen/typehints.py
+++ src/py_img_gen/typehints.py
@@ -1,8 +1,12 @@
 import pathlib
-from typing import Union
+from typing import Annotated, Union
+
+from PIL.Image import Image
 
 PathLike = Union[str, pathlib.Path]
+PilImage = Annotated[Image, "PIL Image"]
 
 __all__ = [
     "PathLike",
+    "PilImage",
 ]
